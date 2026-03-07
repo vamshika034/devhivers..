c18f12e2010b439d2a99d914b6fc03176071f959
vamshika034 <vamshikadevaraj2023@gmail.com>
2026-03-07 23:24:10 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,67 @@
+a = 10
+b = 3
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Remainder:", a % b)
+print("Power:", a ** b)
+
+
+x = 7
+y = 5
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+
+
+
+a = 10
+b = 20
+
+print(a > 5 and b > 15)   # True
+print(a > 15 or b > 15)   # True
+print(not(a > 5))         # False
+
+
+
+
+radius = 7
+area = 3.14 * radius ** 2
+
+print("Area of Circle =", area)
+
+
+P = 1000   # principal
+R = 5      # rate
+T = 2      # time
+
+SI = (P * R * T) / 100
+
+print("Simple Interest =", SI)
+
+
+
+
+
+
+
+a = 10
+b = 20
+c = 30
+
+avg = (a + b + c) / 3
+print("Average =", avg)
+
+
+
+
+
+
