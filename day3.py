3e4496ebe8e0ac7bf631d11c252c4a4b8bb5014c
vamshika034 <vamshikadevaraj2023@gmail.com>
2026-03-07 23:29:16 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,59 @@
+num = 5
+
+if num > 0:
+    print("Number is positive")
+else:
+    print("Number is negative")
+
+
+
+
+
+
+for i in range(1, 6):
+    print(i)
+
+
+
+
+
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i = i + 1
+
+
+
+
+for i in range(1, 11):
+    print(i)
+
+
+
+
+
+ for i in range(1, 51):
+    if i % 2 == 0:
+        print(i)
+
+
+
+
+
+
+sum = 0
+
+for i in range(1, 101):
+    sum = sum + i
+
+print("Sum =", sum)
+
+
+
+
+
+
+
+
