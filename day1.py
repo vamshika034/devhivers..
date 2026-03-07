03184ef17faac34d025be2b5dca0f76a40af2fed
vamshika034 <vamshikadevaraj2023@gmail.com>
2026-03-07 23:09:08 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,44 @@
+print("Hello, World!")
+
+
+
+name = "Vamshika"
+age = 20
+
+
+print(name)
+print(age)
+
+
+age = 21
+print(age)
+
+
+price = 99.50
+print(price)
+
+
+
+name = "Vamshika"
+print(name)
+
+
+is_student = True
+print(is_student)
+
+
+numbers = [10, 20, 30, 40]
+print(numbers)
+
+
+age = 20
+height = 5.6
+name = "Vamshika"
+is_student = True
+marks = [80, 85, 90]
+
+print(age)
+print(height)
+print(name)
+print(is_student)
+print(marks)
