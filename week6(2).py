6b9f96dfa8e4fc8e2e5fd643755c7f05d9cb1671
vamshika034 <vamshikadevaraj2023@gmail.com>
2026-04-12 21:41:37 +0530

    Create week6(2).py

---
+++ week6(2).py
@@ -0,0 +1,17 @@
+# Probability examples
+
+# Probability of rolling a 4
+dice_prob = 1/6
+print("Probability of rolling 4:", dice_prob)
+
+# Probability of heads
+coin_prob = 1/2
+print("Probability of heads:", coin_prob)
+
+# Probability of heads twice
+heads_twice = (1/2) * (1/2)
+print("Probability of heads twice:", heads_twice)
+
+# Probability of even number
+even_prob = 3/6
+print("Probability of even number:", even_prob)
